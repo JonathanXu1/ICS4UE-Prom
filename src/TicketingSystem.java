f10bbe6e310900415623a87268dfd59ad03f566e
Jonathan <jonathanxu25@gmail.com>
2019-02-13 11:53:54 -0500

    more basic setup

--- src/TicketingSystem.java
+++ src/TicketingSystem.java
@@ -1,10 +1,41 @@
+/**
+ Shape Game
+ A basic shape game
+ Jonathan Xu
+ February 6, 2019
+ **/
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+//Keyboard imports
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+//Graphics &GUI imports
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Toolkit;
+import java.awt.Graphics;
+import java.awt.Color;
+
+//Util
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TicketingSystem extends JFrame {
+    static JFrame window;
+    JPanel gamePanel;
 
-    public static void main(String[] args){
-
+    //Main
+    public static void main(String[] args) {
+        window = new TicketingSystem();
     }
+
+    //Constructor - this runs first
+    TicketingSystem() {
+        super("Prom Ticketing System");
+
+    } //End of Constructor
 
 }
