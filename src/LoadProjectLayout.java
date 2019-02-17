b4f6e820b740f24d0a1f5c40bfaa133667a2a522
Dicei <dicei@users.noreply.github.com>
2019-02-16 22:07:37 -0500

    Added class for panels of load and save project

---
+++ src/LoadProjectLayout.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class LoadProjectLayout extends CustomPanel{
+  public LoadProjectLayout(int x, int y){
+        super(x , y, "Dashboard", "Open past projects.");
+        JTextField projectName = new JTextField();
+
+    }
+}
