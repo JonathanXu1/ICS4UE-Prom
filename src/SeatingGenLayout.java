c8fbabfdb628bd31b786f12dee325f24c265f5ef
Dicei <dicei@users.noreply.github.com>
2019-02-21 12:06:25 -0500

    seating gen

--- src/SeatingGenLayout.java
+++ src/SeatingGenLayout.java
@@ -1,10 +1,28 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class SeatingGenLayout extends CustomPanel {
-    public SeatingGenLayout(int x, int y){
-        super(x , y, "Seating Generator", "Group up students into tables.");
+    private FileIOManager io;
+    private ArrayList<Student> students;
 
+    private void loadStudents(){
+        this.students = io.loadStudents();
+       // SeatingAlg seating = new SeatingAlg();
+        // Seating.generateTables();
+    }
+
+    public SeatingGenLayout(int x, int y, FileIOManager io){
+        super(x , y, "Seating Generator", "Group up students into tables.");
+        this.io = io;
         JButton generate = new JButton("Generate Seating!");
         this.add(generate);
+        generate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
