b7e67198954fffafd2fd25baa4b07fdfca942e13
Jonathan <jonathanxu25@gmail.com>
2019-02-26 20:39:51 -0500

    more debugging

--- src/CustomJCheckBox.java
+++ src/CustomJCheckBox.java
@@ -2,10 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CustomJCheckBox extends JCheckBox {
-    CustomJCheckBox(String text){
+    CustomJCheckBox(String text, int x, int y){
         super(text);
         this.setBackground(null);
-        DynamicLabel placeholder = new DynamicLabel("This is a placeholder.", this.getWidth(), this.getHeight(), Color.BLACK);
+        DynamicLabel placeholder = new DynamicLabel("This is a placeholder.", x, y, Color.BLACK);
         this.setFont(placeholder.getFont());
     }
 }
